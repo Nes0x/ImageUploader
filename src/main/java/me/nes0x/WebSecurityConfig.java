f7510b3ab86306e38d3b2df91d3c01846ddcb1a4
Nes0x <przemek882@onet.pl>
2022-03-28 16:23:07 +0200

    Update

--- src/main/java/me/nes0x/WebSecurityConfig.java
+++ src/main/java/me/nes0x/WebSecurityConfig.java
@@ -34,7 +34,6 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/image/add").authenticated()
                 .antMatchers("/image/delete").authenticated()
                 .antMatchers("/register", "/", "/image/**").permitAll()
-                .antMatchers("/user_images", "/user_images/**").denyAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
